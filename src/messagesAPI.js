dfb5e662110b69c2546fa835479d8886a52120ee
aharon amram <aharonam@gmail.com>
2017-07-10 16:33:15 +0300

    add messagesAPI

---
+++ src/messagesAPI.js
@@ -0,0 +1,17 @@
+/**
+ * Created by aharon on 10/07/2017.
+ */
+
+const SERVICE_URL = '',
+        MENU_DATA = '/';
+
+class MessagesAPI {
+
+    constructor() {
+
+    }
+
+    getMenuData() {
+
+    }
+}
