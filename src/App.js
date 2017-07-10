cb7409fedd97fc0db248ce28b9558f739cca41bb
Shavit Cohen <shavit.hit@gmail.com>
2017-07-10 15:41:40 +0300

    fix(): layout

--- src/App.js
+++ src/App.js
@@ -6,26 +6,20 @@ import * as json from './mockData/json.json'
 import VideoIframe from './components/VideoIframe';
 import MdPlayer from './components/mdIframe/mdPlayerComponent';
 
-
 class App extends Component {
-
   render() {
     const input = '# Liron! This is a header\n\nAnd this is a paragraph';
 
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+        <div className="col-xs-3">
+          <Menu items={json.stages} />
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <Menu items={json.stages} />
-        <VideoIframe url={"https://www.youtube.com/embed/k3fz6CC45ok"} width={"100%"} height={"100%"} />
-        <VideoIframe url={"https://player.vimeo.com/video/224808179"} width={"100%"} height={"100%"} />
-        <MdPlayer input={input}></MdPlayer>
-
+        <div className="col-xs-9">
+          <VideoIframe url={"https://www.youtube.com/embed/k3fz6CC45ok"} width={"100%"} height={"100%"} />
+          <VideoIframe url={"https://player.vimeo.com/video/224808179"} width={"100%"} height={"100%"} />
+          <MdPlayer input={input}></MdPlayer>
+        </div>
 
       </div>
     );
